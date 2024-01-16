195b51cafc00c531df1dace271cfa76a136ab24a
Ed Briggler <ed.briggler@jbhunt.com>
2024-01-16 11:34:25 -0600

    i think i had to many async tunnels going

--- server.js
+++ server.js
@@ -13,48 +13,43 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 
-app.get('/api', (req, res) => {
+app.get('/api', async (req, res) => {
   const lat = req.query.lat;
   const lng = req.query.lng;
   const units = req.query.units || 'F';
-
-
-  if ((lat === null || lat === undefined) || (lng === null || lng === undefined)) {
-    res.send({message: 'check query parameters. lat and lng are required.'});
-  }
-
-
   const options = {
     units,
   };
 
-  (async function data() {
-    const data = await Promise.all([
-      getForecastForLatLng(lat, lng),
-      getActiveAlertsForLatLng(lat, lng),
-      getNWSForecast(lat, lng),
-    ]);
-    const pointData = data[0].data;
-    const activeAlerts = data[1].data;
-    const currentConditions = data[2].data;
+  if ((lat === null || lat === undefined) || (lng === null || lng === undefined)) {
+    res.send({message: 'check query parameters. lat and lng are required.'});
+  }
+  
+  const data = await Promise.all([
+    getForecastForLatLng(lat, lng),
+    getActiveAlertsForLatLng(lat, lng),
+    getNWSForecast(lat, lng),
+  ]);
+  const pointData = data[0].data;
+  const activeAlerts = data[1].data;
+  const currentConditions = data[2].data;
 
-    const forecastData = await Promise.all([
-      getForecast(pointData.properties),
-      getHourlyForecast(pointData.properties),
-    ]);
+  const forecastData = await Promise.all([
+    getForecast(pointData.properties),
+    getHourlyForecast(pointData.properties),
+  ]);
 
-    const forecast = forecastData[0].data;
-    const hourlyForecast = forecastData[1].data;
+  const forecast = forecastData[0].data;
+  const hourlyForecast = forecastData[1].data;
 
-    const response = {
-      location: currentConditions.location,
-      currentObservation: currentConditions.currentobservation,
-      days: mapDailyForecastPeriods(forecast.properties.periods, hourlyForecast.properties.periods, options),
-      alerts: mapAlerts(activeAlerts.features, options),
-    }
+  const response = {
+    location: currentConditions.location,
+    currentObservation: currentConditions.currentobservation,
+    days: mapDailyForecastPeriods(forecast.properties.periods, hourlyForecast.properties.periods, options),
+    alerts: mapAlerts(activeAlerts.features, options),
+  }
 
-    res.send(response);
-  })();
+  res.send(response);
 });
 
 function onSignal() {
