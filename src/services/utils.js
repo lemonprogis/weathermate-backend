5de7f66c68dd8a0064876c2fb2421c553cadd47e
Ed Briggler <ed.briggler@jbhunt.com>
2024-02-10 14:53:06 -0600

    updating times and list items for hourly

--- src/services/utils.js
+++ src/services/utils.js
@@ -41,8 +41,37 @@ function feelsLike(co) {
   return '--';
 }
 
+function convertUTCToLocalTime(dateString) {
+  return new Date(dateString);
+}
+
+function timeConvert(time) {
+  // Check correct time format and split into components
+  time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time,];
+
+  if (time.length > 1) {
+    // If time format correct
+    time = time.slice(1); // Remove full string match value
+    time[5] = +time[0] < 12 ? 'am' : 'pm'; // Set AM/PM
+    time[0] = +time[0] % 12 || 12; // Adjust hours
+  }
+  return time.join(''); // return adjusted time or original string
+}
+
+function getMonthName(dt) {
+  const md = dt.slice(5).split('-');
+  // console.log(md);
+  const date = new Date();
+  date.setMonth(md[0] - 1);
+
+  return `${date.toLocaleString('en-US', {month: 'long'})} ${md[1]}`;
+}
+
 module.exports = {
   toFahrenheit,
   getWindDirection,
   feelsLike,
+  timeConvert,
+  convertUTCToLocalTime,
+  getMonthName,
 };
