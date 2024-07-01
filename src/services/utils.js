a10a0cfe6046d8486c1ba1170601ef87a5e2e390
lemonprogis <edward.briggler@gmail.com>
2024-06-30 20:38:52 -0500

    fix up our utils

--- src/services/utils.js
+++ src/services/utils.js
@@ -2,8 +2,8 @@ function toFahrenheit(temp) {
   if (temp === null) {
     return '--';
   }
-  return `${Math.round((temp.value * 1.8 + 32).toFixed(1))}°`;
-};
+  return `${Math.round(temp.value * 1.8 + 32).toFixed(0)}°`;
+}
 
 function getWindDirection(num) {
   if (num === null || num === 0) {
@@ -29,7 +29,7 @@ function getWindDirection(num) {
     'NNW',
   ];
   return arr[val % 16];
-};
+}
 
 function feelsLike(co) {
   if (co.WindChill !== undefined)
@@ -41,37 +41,8 @@ function feelsLike(co) {
   return '--';
 }
 
-function convertUTCToLocalTime(dateString) {
-  return new Date(dateString);
-}
-
-function timeConvert(time) {
-  // Check correct time format and split into components
-  time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time,];
-
-  if (time.length > 1) {
-    // If time format correct
-    time = time.slice(1); // Remove full string match value
-    time[5] = +time[0] < 12 ? 'am' : 'pm'; // Set AM/PM
-    time[0] = +time[0] % 12 || 12; // Adjust hours
-  }
-  return time.join(''); // return adjusted time or original string
-}
-
-function getMonthName(dt) {
-  const md = dt.slice(5).split('-');
-  // console.log(md);
-  const date = new Date();
-  date.setMonth(md[0] - 1);
-
-  return `${date.toLocaleString('en-US', {month: 'long'})} ${md[1]}`;
-}
-
 module.exports = {
   toFahrenheit,
   getWindDirection,
   feelsLike,
-  timeConvert,
-  convertUTCToLocalTime,
-  getMonthName,
 };
